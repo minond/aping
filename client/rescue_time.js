75e700730929e6aed673f7bc19b3b6d3885df41f
Marcos Minond <minond.marcos@gmail.com>
2014-12-04 17:46:23 -0700

    fixing indentation

--- client/rescue_time.js
+++ client/rescue_time.js
@@ -1,17 +1,18 @@
 'use strict';
 
-var URL_SELECT = '/anapi/data?format=json&version=0&key=${ fields.token }&' +
-        'perspective=${ query.perspective }&' +
-        'resolution_time=${ query.resolution_time}&' +
-        'restrict_group=${ query.restrict_group }&' +
-        'restrict_user=${ query.restrict_user }&' +
-        'restrict_begin=<%= format.date.iso(since) %>&' +
-        'restrict_end=<%= format.date.iso(until) %>&' +
-        'restrict_kind=${ query.restrict_kind }&' +
-        'restrict_project${ query.restrict_project }&' +
-        'restrict_thing=${ query.restrict_thing }&' +
-        'restrict_thingy=${ query.restrict_thingy }&' +
-        'operation=select';
+var URL_SELECT = '/anapi/data?' +
+    'format=json&version=0&key=${ fields.token }&' +
+    'perspective=${ query.perspective }&' +
+    'resolution_time=${ query.resolution_time}&' +
+    'restrict_group=${ query.restrict_group }&' +
+    'restrict_user=${ query.restrict_user }&' +
+    'restrict_begin=<%= format.date.iso(since) %>&' +
+    'restrict_end=<%= format.date.iso(until) %>&' +
+    'restrict_kind=${ query.restrict_kind }&' +
+    'restrict_project${ query.restrict_project }&' +
+    'restrict_thing=${ query.restrict_thing }&' +
+    'restrict_thingy=${ query.restrict_thingy }&' +
+    'operation=select';
 
 var aping = require('../src/aping');
 
