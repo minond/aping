2be10663e31850383a23a3b5b76eab5030a7bd71
Marcos Minond <minond.marcos@gmail.com>
2014-12-14 00:52:37 -0700

    working on unit tests

--- test/requests_test.js
+++ test/requests_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('requests', function () {
-    var proxy, request;
+    var proxy, request, options, me;
 
     var assert = require('assert'),
         requests = require('../src/requests'),
@@ -9,12 +9,30 @@ describe('requests', function () {
         oauth = requests.oauth_request,
         oauth2 = requests.oauth2_request;
 
+    beforeEach(function () {
+        me = {
+            $fields: {},
+            $request_config: {},
+            emit: function () {
+                return;
+            }
+        };
+    });
+
     describe('#make_url', function () {
         it('makes a freaking url', function () {
             assert.equal(
                 'http://google.com/search',
                 requests.$make_url('http', 'google.com', 'search')
             );
+        });
+    });
+
+    describe('#gen_options', function () {
+        it('uses $fields as default', function () {
+            me.$fields.name = 'Marcos';
+            options = requests.$gen_options(me, 'hi ${name}');
+            assert.equal(options.path, 'hi Marcos');
         });
     });
 });
