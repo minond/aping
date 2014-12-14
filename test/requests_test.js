2970e1ae0dcfef663531e2dcaafea57a81136c72
Marcos Minond <minond.marcos@gmail.com>
2014-12-14 01:07:25 -0700

    testing resolve method

--- test/requests_test.js
+++ test/requests_test.js
@@ -1,13 +1,18 @@
 'use strict';
 
 describe('requests', function () {
-    var proxy, request, options, me;
+    var proxy, request, options, me, deferred;
 
-    var assert = require('assert'),
+    var Q = require('q'),
+        assert = require('assert'),
         requests = require('../src/requests'),
         http = requests.http_request,
         oauth = requests.oauth_request,
         oauth2 = requests.oauth2_request;
+
+    function log() {
+        return;
+    }
 
     beforeEach(function () {
         me = {
@@ -45,6 +50,27 @@ describe('requests', function () {
             me.$fields.name = 'Marcos';
             options = requests.$gen_options(me, 'hi ${name}', { name: 'Andres' });
             assert.equal(options.path, 'hi Andres');
+        });
+    });
+
+    describe('#resolve', function () {
+        it('resolves when json is valid', function (done) {
+            deferred = Q.defer();
+            deferred.promise.then(function (obj) {
+                assert(obj.a);
+                done();
+            });
+
+            requests.$resolve(deferred, ['{"a":true}'], log)();
+        });
+
+        it('rejects when json is invalid', function (done) {
+            deferred = Q.defer();
+            deferred.promise.then(function () {}, function (obj) {
+                done();
+            });
+
+            requests.$resolve(deferred, ['{:true}'], log)();
         });
     });
 });
